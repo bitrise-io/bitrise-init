00485167d3bb67a934745f716736bcf9a84ba467
Krisztian Godrei <godreikrisztian@gmail.com>
2017-07-11 13:03:18 +0200

    code style

---
+++ models/configbuilder.go
@@ -0,0 +1,177 @@
+package models
+
+import (
+	"errors"
+
+	bitriseModels "github.com/bitrise-io/bitrise/models"
+	envmanModels "github.com/bitrise-io/envman/models"
+)
+
+// WorkflowID ...
+type WorkflowID string
+
+const (
+	// PrimaryWorkflowID ...
+	PrimaryWorkflowID WorkflowID = "primary"
+	// DeployWorkflowID ...
+	DeployWorkflowID WorkflowID = "deploy"
+
+	// FormatVersion ...
+	FormatVersion = bitriseModels.Version
+
+	defaultSteplibSource = "https://github.com/bitrise-io/bitrise-steplib.git"
+)
+
+// ConfigBuilderModel ...
+type ConfigBuilderModel struct {
+	workflowBuilderMap map[WorkflowID]*workflowBuilderModel
+}
+
+// NewDefaultConfigBuilder ...
+func NewDefaultConfigBuilder(isIncludeCache bool) *ConfigBuilderModel {
+	return &ConfigBuilderModel{
+		workflowBuilderMap: map[WorkflowID]*workflowBuilderModel{
+			PrimaryWorkflowID: newDefaultWorkflowBuilder(isIncludeCache),
+		},
+	}
+}
+
+// AddDefaultWorkflowBuilder ...
+func (builder *ConfigBuilderModel) AddDefaultWorkflowBuilder(workflow WorkflowID, isIncludeCache bool) {
+	builder.workflowBuilderMap[workflow] = newDefaultWorkflowBuilder(isIncludeCache)
+}
+
+// AppendPreparStepListTo ...
+func (builder *ConfigBuilderModel) AppendPreparStepListTo(workflow WorkflowID, items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[workflow]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[workflow] = workflowBuilder
+	}
+	workflowBuilder.appendPreparStepList(items...)
+}
+
+// AppendDependencyStepListTo ...
+func (builder *ConfigBuilderModel) AppendDependencyStepListTo(workflow WorkflowID, items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[workflow]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[workflow] = workflowBuilder
+	}
+	workflowBuilder.appendDependencyStepList(items...)
+}
+
+// AppendMainStepListTo ...
+func (builder *ConfigBuilderModel) AppendMainStepListTo(workflow WorkflowID, items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[workflow]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[workflow] = workflowBuilder
+	}
+	workflowBuilder.appendMainStepList(items...)
+}
+
+// AppendDeployStepListTo ...
+func (builder *ConfigBuilderModel) AppendDeployStepListTo(workflow WorkflowID, items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[workflow]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[workflow] = workflowBuilder
+	}
+	workflowBuilder.appendDeployStepList(items...)
+}
+
+// AppendPreparStepList ...
+func (builder *ConfigBuilderModel) AppendPreparStepList(items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[PrimaryWorkflowID]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[PrimaryWorkflowID] = workflowBuilder
+	}
+	workflowBuilder.appendPreparStepList(items...)
+}
+
+// AppendDependencyStepList ...
+func (builder *ConfigBuilderModel) AppendDependencyStepList(items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[PrimaryWorkflowID]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[PrimaryWorkflowID] = workflowBuilder
+	}
+	workflowBuilder.appendDependencyStepList(items...)
+}
+
+// AppendMainStepList ...
+func (builder *ConfigBuilderModel) AppendMainStepList(items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[PrimaryWorkflowID]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[PrimaryWorkflowID] = workflowBuilder
+	}
+	workflowBuilder.appendMainStepList(items...)
+}
+
+// AppendDeployStepList ...
+func (builder *ConfigBuilderModel) AppendDeployStepList(items ...bitriseModels.StepListItemModel) {
+	workflowBuilder := builder.workflowBuilderMap[PrimaryWorkflowID]
+	if workflowBuilder == nil {
+		workflowBuilder = &workflowBuilderModel{}
+		builder.workflowBuilderMap[PrimaryWorkflowID] = workflowBuilder
+	}
+	workflowBuilder.appendDeployStepList(items...)
+}
+
+// Merge ...
+func (builder ConfigBuilderModel) Merge(configBuilder ConfigBuilderModel) ConfigBuilderModel {
+	workflowIDs := []WorkflowID{}
+	for workflowID := range builder.workflowBuilderMap {
+		workflowIDs = append(workflowIDs, workflowID)
+	}
+
+	for _, workfloID := range workflowIDs {
+		toMergeWorkflowBuilder, ok := configBuilder.workflowBuilderMap[workfloID]
+		if ok {
+			originalWorkflowBuilder := builder.workflowBuilderMap[workfloID]
+			originalWorkflowBuilder.merge(toMergeWorkflowBuilder)
+		}
+	}
+
+	return builder
+}
+
+// Generate ...
+func (builder *ConfigBuilderModel) Generate(projectType string, appEnvs ...envmanModels.EnvironmentItemModel) (bitriseModels.BitriseDataModel, error) {
+	primaryWorkflowBuilder, ok := builder.workflowBuilderMap[PrimaryWorkflowID]
+	if !ok || primaryWorkflowBuilder == nil || len(primaryWorkflowBuilder.stepList()) == 0 {
+		return bitriseModels.BitriseDataModel{}, errors.New("primary workflow not defined")
+	}
+
+	workflows := map[string]bitriseModels.WorkflowModel{}
+	for workflowID, workflowBuilder := range builder.workflowBuilderMap {
+		workflows[string(workflowID)] = workflowBuilder.generate()
+	}
+
+	triggerMap := []bitriseModels.TriggerMapItemModel{
+		bitriseModels.TriggerMapItemModel{
+			PushBranch: "*",
+			WorkflowID: string(PrimaryWorkflowID),
+		},
+		bitriseModels.TriggerMapItemModel{
+			PullRequestSourceBranch: "*",
+			WorkflowID:              string(PrimaryWorkflowID),
+		},
+	}
+
+	app := bitriseModels.AppModel{
+		Environments: appEnvs,
+	}
+
+	return bitriseModels.BitriseDataModel{
+		FormatVersion:        FormatVersion,
+		DefaultStepLibSource: defaultSteplibSource,
+		ProjectType:          projectType,
+		TriggerMap:           triggerMap,
+		Workflows:            workflows,
+		App:                  app,
+	}, nil
+}
---
+++ models/workflowbuilder.go
@@ -0,0 +1,135 @@
+package models
+
+import (
+	"github.com/bitrise-core/bitrise-init/steps"
+	bitriseModels "github.com/bitrise-io/bitrise/models"
+)
+
+type workflowBuilderModel struct {
+	PrepareSteps    []bitriseModels.StepListItemModel
+	DependencySteps []bitriseModels.StepListItemModel
+	MainSteps       []bitriseModels.StepListItemModel
+	DeploySteps     []bitriseModels.StepListItemModel
+
+	steps []bitriseModels.StepListItemModel
+}
+
+func newDefaultWorkflowBuilder(isIncludeCache bool) *workflowBuilderModel {
+	return &workflowBuilderModel{
+		PrepareSteps:    steps.DefaultPrepareStepList(isIncludeCache),
+		DependencySteps: []bitriseModels.StepListItemModel{},
+		MainSteps:       []bitriseModels.StepListItemModel{},
+		DeploySteps:     steps.DefaultDeployStepList(isIncludeCache),
+	}
+}
+
+func newWorkflowBuilder(items ...bitriseModels.StepListItemModel) *workflowBuilderModel {
+	return &workflowBuilderModel{
+		steps: items,
+	}
+}
+
+func (builder *workflowBuilderModel) appendPreparStepList(items ...bitriseModels.StepListItemModel) {
+	builder.PrepareSteps = append(builder.PrepareSteps, items...)
+}
+
+func (builder *workflowBuilderModel) appendDependencyStepList(items ...bitriseModels.StepListItemModel) {
+	builder.DependencySteps = append(builder.DependencySteps, items...)
+}
+
+func (builder *workflowBuilderModel) appendMainStepList(items ...bitriseModels.StepListItemModel) {
+	builder.MainSteps = append(builder.MainSteps, items...)
+}
+
+func (builder *workflowBuilderModel) appendDeployStepList(items ...bitriseModels.StepListItemModel) {
+	builder.DeploySteps = append(builder.DeploySteps, items...)
+}
+
+func (builder *workflowBuilderModel) stepList() []bitriseModels.StepListItemModel {
+	if len(builder.steps) > 0 {
+		return builder.steps
+	}
+
+	stepList := []bitriseModels.StepListItemModel{}
+	stepList = append(stepList, builder.PrepareSteps...)
+	stepList = append(stepList, builder.DependencySteps...)
+	stepList = append(stepList, builder.MainSteps...)
+	stepList = append(stepList, builder.DeploySteps...)
+	return stepList
+}
+
+func (builder *workflowBuilderModel) generate() bitriseModels.WorkflowModel {
+	return bitriseModels.WorkflowModel{
+		Steps: builder.stepList(),
+	}
+}
+
+func (builder *workflowBuilderModel) merge(workflowBuilder *workflowBuilderModel) {
+	for _, stepListItemToCheck := range workflowBuilder.PrepareSteps {
+		contains := false
+		for _, stepListItem := range builder.PrepareSteps {
+			if stepListItemEquals(stepListItem, stepListItemToCheck) {
+				contains = true
+				break
+			}
+		}
+		if !contains {
+			builder.appendPreparStepList(stepListItemToCheck)
+		}
+	}
+
+	for _, stepListItemToCheck := range workflowBuilder.DependencySteps {
+		contains := false
+		for _, stepListItem := range builder.DependencySteps {
+			if stepListItemEquals(stepListItem, stepListItemToCheck) {
+				contains = true
+				break
+			}
+		}
+		if !contains {
+			builder.appendPreparStepList(stepListItemToCheck)
+		}
+	}
+
+	for _, stepListItemToCheck := range workflowBuilder.MainSteps {
+		contains := false
+		for _, stepListItem := range builder.MainSteps {
+			if stepListItemEquals(stepListItem, stepListItemToCheck) {
+				contains = true
+				break
+			}
+		}
+		if !contains {
+			builder.appendPreparStepList(stepListItemToCheck)
+		}
+	}
+
+	for _, stepListItemToCheck := range workflowBuilder.DeploySteps {
+		contains := false
+		for _, stepListItem := range builder.DeploySteps {
+			if stepListItemEquals(stepListItem, stepListItemToCheck) {
+				contains = true
+				break
+			}
+		}
+		if !contains {
+			builder.appendPreparStepList(stepListItemToCheck)
+		}
+	}
+}
+
+func stepListItemEquals(stepListItem1, stepListItem2 bitriseModels.StepListItemModel) bool {
+	stepID1 := ""
+	for key := range stepListItem1 {
+		stepID1 = key
+		break
+	}
+
+	stepID2 := ""
+	for key := range stepListItem2 {
+		stepID2 = key
+		break
+	}
+
+	return stepID1 == stepID2
+}
