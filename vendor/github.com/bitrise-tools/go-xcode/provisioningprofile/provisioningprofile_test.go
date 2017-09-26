d1dab1b004761d4e1d34e81de4edfd376e490d41
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-09-26 10:06:11 +0200

    move to dep

---
+++ vendor/github.com/bitrise-tools/go-xcode/provisioningprofile/provisioningprofile_test.go
@@ -0,0 +1,69 @@
+package provisioningprofile
+
+import (
+	"testing"
+
+	"github.com/bitrise-tools/go-xcode/exportoptions"
+	"github.com/bitrise-tools/go-xcode/plistutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetExportMethod(t *testing.T) {
+	t.Log("development profile specifies development export method")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(developmentProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, exportoptions.MethodDevelopment, GetExportMethod(profile))
+	}
+
+	t.Log("app store profile specifies app-store export method")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(appStoreProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, exportoptions.MethodAppStore, GetExportMethod(profile))
+	}
+
+	t.Log("ad hoc profile specifies ad-hoc export method")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(adHocProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, exportoptions.MethodAdHoc, GetExportMethod(profile))
+	}
+
+	t.Log("it creates model from enterprise profile content")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(enterpriseProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, exportoptions.MethodEnterprise, GetExportMethod(profile))
+	}
+}
+
+func TestGetDeveloperTeam(t *testing.T) {
+	t.Log("development profile specifies development export method")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(developmentProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, "9NS44DLTN7", GetDeveloperTeam(profile))
+	}
+
+	t.Log("app store profile specifies app-store export method")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(appStoreProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, "9NS44DLTN7", GetDeveloperTeam(profile))
+	}
+
+	t.Log("ad hoc profile specifies ad-hoc export method")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(adHocProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, "9NS44DLTN7", GetDeveloperTeam(profile))
+	}
+
+	t.Log("it creates model from enterprise profile content")
+	{
+		profile, err := plistutil.NewPlistDataFromContent(enterpriseProfileContent)
+		require.NoError(t, err)
+		require.Equal(t, "PF3BP78LQ8", GetDeveloperTeam(profile))
+	}
+}
