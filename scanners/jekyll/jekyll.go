d292bfd1217140582b44a4f4dcff418f2e21eb66
vgaidarji <veaceslav.gaidarji@gmail.com>
2018-02-16 10:43:25 +0200

    Implement Jekyll project type files search

--- scanners/jekyll/jekyll.go
+++ scanners/jekyll/jekyll.go
@@ -1,7 +1,11 @@
 package jekyll
 
 import (
+	"fmt"
+
 	"github.com/bitrise-core/bitrise-init/models"
+	"github.com/bitrise-core/bitrise-init/utility"
+	"github.com/bitrise-io/go-utils/log"
 )
 
 // Scanner ...
@@ -20,7 +24,36 @@ func (Scanner) Name() string {
 
 // DetectPlatform ...
 func (scanner *Scanner) DetectPlatform(searchDir string) (bool, error) {
-	return false, nil
+	fileList, err := utility.ListPathInDirSortedByComponents(searchDir, true)
+	if err != nil {
+		return false, fmt.Errorf("failed to search for files in (%s), error: %s", searchDir, err)
+	}
+
+	// Search for _config.yml file
+	// Note: hexo.io also uses _config.yml, but package.json for dependencies.
+	// This means having config file is not enough to detect Jekyll platform.
+	// _config.yml + Gemfile (with jekyll gem) = Jekyll platform
+	log.TInfof("Searching for %s file", configYmlFile)
+	configYmlPath, err := filterProjectFile(configYmlFile, fileList)
+	if err != nil {
+		return false, fmt.Errorf("failed to search for %s file, error: %s", configYmlFile, err)
+	}
+	log.TPrintf("%s found", configYmlFile)
+
+	// Search for Jekyll in Gemfile
+	log.TInfof("Searching for %s file", gemfile)
+	gemfilePath, err := filterProjectFile(gemfile, fileList)
+	if err != nil {
+		return false, fmt.Errorf("failed to search for %s file, error: %s", gemfile, err)
+	}
+	log.TPrintf("%s found", gemfile)
+
+	if configYmlPath == "" || gemfilePath == "" {
+		log.TPrintf("platform not detected")
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // ExcludedScannerNames ...
--- scanners/jekyll/utility.go
+++ scanners/jekyll/utility.go
@@ -1,6 +1,27 @@
 package jekyll
 
+import (
+	"github.com/bitrise-core/bitrise-init/utility"
+)
+
 const (
 	// ScannerName ...
 	ScannerName = "jekyll"
+	configYmlFile = "_config.yml"
+	gemfile = "Gemfile"
 )
+
+// filterProjectFile ...
+func filterProjectFile(fileName string, fileList []string) (string, error) {
+	allowGivenFileBaseFilter := utility.BaseFilter(fileName, true)
+	filePaths, err := utility.FilterPaths(fileList, allowGivenFileBaseFilter)
+	if err != nil {
+		return "", err
+	}
+
+	if len(filePaths) == 0 {
+		return "", nil
+	}
+
+	return filePaths[0], nil
+}
