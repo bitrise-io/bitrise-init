11deb68a208c79ebc192efbe2d6916336c26a7a8
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-01-15 16:53:15 +0100

    Tests added

--- toolscanner/toolscanner.go
+++ toolscanner/toolscanner.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
-	"log"
 
 	"github.com/bitrise-core/bitrise-init/models"
 	"github.com/bitrise-core/bitrise-init/utility"
@@ -17,32 +16,30 @@ const (
 	ProjectTypeTemplateKey = "PROJECT_TYPE"
 )
 
-// AddProjectType is used to add a project type for automation tool scanners's option tree and config map
-func AddProjectType(scannerOptionTree models.OptionNode, scannerConfigMap models.BitriseConfigMap, detectedProjectTypes []string) (models.OptionNode, models.BitriseConfigMap, error) {
-	log.Printf("AddProjectTypeToToolScanner old toolScannerOptionTree: %s, \n scannerConfigMap %s, \n detectedProjectTypes: %s",
-		scannerOptionTree, scannerConfigMap, detectedProjectTypes)
-
-	// For each tool scanner generated config, multiply it to get all combinations of 'Existing config' X 'Detected project type'
+// AddProjectTypeToConfig get cartesian product: 'Existing tool scanner generated config' X 'Detected project type'
+func AddProjectTypeToConfig(scannerConfigMap models.BitriseConfigMap, detectedProjectTypes []string) (models.BitriseConfigMap, error) {
 	configMapWithProjecTypes := map[string]string{}
 	for _, projectType := range detectedProjectTypes {
 		for configName, config := range scannerConfigMap {
-			configWithProjectType, err := evaluateConfigTemplate(config, map[string]string{ProjectTypeTemplateKey: projectType})
+			configWithProjectType, err := evaluateConfigTemplate(config,
+				map[string]string{ProjectTypeTemplateKey: projectType})
 			if err != nil {
-				return scannerOptionTree, scannerConfigMap, fmt.Errorf("failed to add project type to tool scanner bitrise.yml, error: %s", err)
+				return nil,
+					fmt.Errorf("failed to add project type to tool scanner bitrise.yml, error: %s", err)
 			}
 			configMapWithProjecTypes[appendProjectTypeToConfigName(configName, projectType)] = configWithProjectType
 		}
 	}
+	return configMapWithProjecTypes, nil
+}
 
-	// add the possible project types as a question to the option map
+// AddProjectTypeToOptions adds a project type question to automation tool scanners's option tree
+func AddProjectTypeToOptions(scannerOptionTree models.OptionNode, detectedProjectTypes []string) models.OptionNode {
 	optionsTreeWithProjectTypeRoot := models.NewOption(ProjectTypeUserTitle, ProjectTypeTemplateKey)
 	for _, projectType := range detectedProjectTypes {
 		optionsTreeWithProjectTypeRoot.AddOption(projectType, appendProjectTypeToConfig(scannerOptionTree, projectType))
 	}
-
-	log.Printf("AddProjectTypeToToolScanner new options root: %s, \n new config map: %s",
-		optionsTreeWithProjectTypeRoot, configMapWithProjecTypes)
-	return *optionsTreeWithProjectTypeRoot, configMapWithProjecTypes, nil
+	return *optionsTreeWithProjectTypeRoot
 }
 
 func evaluateConfigTemplate(configStr string, substitutions map[string]string) (string, error) {
