c20cf3a723e90a237670291c2192d3fad4a2ca80
Papik Tamas <trapacska@gmail.com>
2019-06-12 15:59:02 +0200

    dep update and added 4 types of input

--- scanner/utils.go
+++ scanner/utils.go
@@ -3,6 +3,7 @@ package scanner
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 
 	yaml "gopkg.in/yaml.v2"
@@ -13,30 +14,88 @@ import (
 	"github.com/bitrise-io/goinp/goinp"
 )
 
+func getDefaultValue(opt models.OptionNode) string {
+	if opt.Type == models.TypeOptionalSelector {
+		return ""
+	}
+
+	for key := range opt.ChildOptionMap {
+		return key
+	}
+	return ""
+}
+
+func getOptions(opts map[string]*models.OptionNode) (options []string) {
+	for key := range opts {
+		options = append(options, key)
+	}
+	return
+}
+
+func selectOption(options []string) (string, error) {
+	for i, option := range options {
+		fmt.Printf("[%d] : %s\n", i+1, option)
+	}
+	fmt.Printf("Type in the option's number, then hit Enter: ")
+
+	answer, err := goinp.AskForOptionalInput("", false)
+	if err != nil {
+		return "", err
+	}
+
+	optionNo, err := strconv.Atoi(strings.TrimSpace(answer))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse option number, pick a number from 1-%d", len(options))
+	}
+
+	if optionNo-1 < 0 || optionNo-1 >= len(options) {
+		return "", fmt.Errorf("invalid option number, pick a number from 1-%d", len(options))
+	}
+
+	return options[optionNo-1], nil
+}
+
 func askForOptionValue(option models.OptionNode) (string, string, error) {
+	const customValueOptionText = "<custom value>"
+
 	// this options is a last element in a tree, contains only config name
 	if option.Config != "" {
 		return "", option.Config, nil
 	}
 
-	var (
-		answer string
-		err    error
-	)
+	optional := option.Type == models.TypeOptionalUserInput || option.Type == models.TypeOptionalSelector
 
 	switch option.Type {
 	case models.TypeSelector, models.TypeOptionalSelector:
-		answer, err = goinp.AskOptions(option.Title, "", option.Type == models.TypeOptionalSelector, option.GetValues()...)
-	case models.TypeUserInput, models.TypeOptionalUserInput:
-		var defaultValue string
-		for key := range option.ChildOptionMap {
-			defaultValue = key
-			break
+		fmt.Println("Select \"" + option.Title + "\" from the list:")
+
+		options := getOptions(option.ChildOptionMap)
+		if optional {
+			options = append(options, customValueOptionText)
 		}
-		answer, err = goinp.AskOptions(option.Title, defaultValue, option.Type == models.TypeOptionalUserInput)
+
+		selected, err := selectOption(options)
+		if err != nil {
+			return "", "", err
+		}
+
+		if option.Type == models.TypeSelector || selected != customValueOptionText {
+			return option.EnvKey, selected, nil
+		}
+
+		fallthrough
+	case models.TypeUserInput, models.TypeOptionalUserInput:
+		suffix := ": "
+		if optional {
+			suffix = " (optional): "
+		}
+		fmt.Print("Enter value for \"" + option.Title + "\"" + suffix)
+
+		answer, err := goinp.AskForOptionalInput(getDefaultValue(option), optional)
+		return option.EnvKey, strings.TrimSpace(answer), err
 	}
 
-	return option.EnvKey, strings.TrimSpace(answer), err
+	return "", "", fmt.Errorf("invalid input type")
 }
 
 // AskForOptions ...
@@ -71,11 +130,17 @@ func AskForOptions(options models.OptionNode) (string, []envmanModels.Environmen
 			}
 		} else {
 			// go to the next option, based on the selected value
-			childOptions, found := opt.ChildOptionMap[selectedValue]
+			childOption, found := opt.ChildOptionMap[selectedValue]
 			if !found {
-				return nil
+				if opt.Type != models.TypeOptionalSelector {
+					return nil
+				}
+				for _, option := range opt.ChildOptionMap {
+					childOption = option
+					break
+				}
 			}
-			nestedOptions = childOptions
+			nestedOptions = childOption
 		}
 
 		return walkDepth(*nestedOptions)
@@ -108,8 +173,9 @@ func AskForConfig(scanResult models.ScanResultModel) (bitriseModels.BitriseDataM
 	} else if len(platforms) == 1 {
 		platform = platforms[0]
 	} else {
+		fmt.Println("Select platform:")
 		var err error
-		platform, err = goinp.AskOptions("platform", "", false, platforms...)
+		platform, err = selectOption(platforms)
 		if err != nil {
 			return bitriseModels.BitriseDataModel{}, err
 		}
--- scanners/android/android.go
+++ scanners/android/android.go
@@ -91,16 +91,109 @@ func (scanner *Scanner) Options() (models.OptionNode, models.Warnings, models.Ic
 
 // DefaultOptions ...
 func (scanner *Scanner) DefaultOptions() models.OptionNode {
-	projectLocationOption := models.NewOption(ProjectLocationInputTitle, ProjectLocationInputEnvKey).SetType(models.TypeUserInput)
-	moduleOption := models.NewOption(ModuleInputTitle, ModuleInputEnvKey).SetType(models.TypeUserInput)
-	variantOption := models.NewOption(VariantInputTitle, VariantInputEnvKey).SetType(models.TypeOptionalUserInput)
-	configOption := models.NewConfigOption(DefaultConfigName, nil)
+	// projectLocationOption := models.NewOption(ProjectLocationInputTitle, ProjectLocationInputEnvKey).SetType(models.TypeUserInput)
+	// moduleOption := models.NewOption(ModuleInputTitle, ModuleInputEnvKey).SetType(models.TypeUserInput)
+	// variantOption := models.NewOption(VariantInputTitle, VariantInputEnvKey).SetType(models.TypeOptionalUserInput)
+	// configOption := models.NewConfigOption(DefaultConfigName, nil)
 
-	projectLocationOption.AddOption("", moduleOption)
-	moduleOption.AddOption("", variantOption)
-	variantOption.AddConfig("", configOption)
+	// projectLocationOption.AddOption("", moduleOption)
+	// moduleOption.AddOption("", variantOption)
+	// variantOption.AddConfig("", configOption)
 
-	return *projectLocationOption
+	// return *projectLocationOption
+
+	// testing options
+
+	/*
+
+		The section below is for client side testing only, should be removed for production use.
+
+	*/
+
+	q1 := models.NewOption("title1-TypeUserInput", "envkey1").SetType(models.TypeUserInput)
+
+	q2 := models.NewOption("title2-TypeOptionalUserInput", "envkey2").SetType(models.TypeOptionalUserInput)
+
+	q3 := models.NewOption("title3-TypeUserInput-default-value", "envkey3").SetType(models.TypeUserInput)
+
+	q4 := models.NewOption("title4-TypeOptionalUserInput-default-value", "envkey4").SetType(models.TypeOptionalUserInput)
+
+	q5 := models.NewOption("title5-TypeSelectore-one-item", "envkey5").SetType(models.TypeSelector)
+
+	q6 := models.NewOption("title6-TypeOptionalSelector-one-item", "envkey6").SetType(models.TypeOptionalSelector)
+
+	q7 := models.NewOption("title7-TypeSelector-3-item", "envkey7").SetType(models.TypeSelector)
+	q8 := models.NewOption("title8-TypeSelector-3-item", "envkey8").SetType(models.TypeSelector)
+	q9 := models.NewOption("title9-TypeSelector-3-item", "envkey9").SetType(models.TypeSelector)
+
+	q10 := models.NewOption("title10-TypeOptionalSelector-3-item", "envkey10").SetType(models.TypeOptionalSelector)
+	q11 := models.NewOption("title11-TypeOptionalSelector-3-item", "envkey11").SetType(models.TypeOptionalSelector)
+	q12 := models.NewOption("title12-TypeOptionalSelector-3-item", "envkey12").SetType(models.TypeOptionalSelector)
+
+	q10a := models.NewOption("title10a-TypeOptionalSelector-3-item", "envkey10a").SetType(models.TypeOptionalSelector)
+	q11a := models.NewOption("title11a-TypeOptionalSelector-3-item", "envkey11a").SetType(models.TypeOptionalSelector)
+	q12a := models.NewOption("title12a-TypeOptionalSelector-3-item", "envkey12a").SetType(models.TypeOptionalSelector)
+
+	q10b := models.NewOption("title10b-TypeOptionalSelector-3-item", "envkey10b").SetType(models.TypeOptionalSelector)
+	q11b := models.NewOption("title11b-TypeOptionalSelector-3-item", "envkey11b").SetType(models.TypeOptionalSelector)
+	q12b := models.NewOption("title12b-TypeOptionalSelector-3-item", "envkey12b").SetType(models.TypeOptionalSelector)
+
+	q1.AddOption("", q2)
+	q2.AddOption("", q3)
+	q3.AddOption("my default value", q4)
+	q4.AddOption("my default value", q5)
+	q5.AddOption("my only option", q6)
+	q6.AddOption("my only option", q7)
+
+	//
+	q10.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q10.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q10.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q7.AddOption("first option", q10)
+
+	q11.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q11.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q11.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q7.AddOption("second option", q11)
+
+	q12.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q12.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q12.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q7.AddOption("third option", q12)
+
+	//
+	q10a.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q10a.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q10a.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q8.AddOption("first option", q10a)
+
+	q11a.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q11a.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q11a.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q8.AddOption("second option", q11a)
+
+	q12a.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q12a.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q12a.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q8.AddOption("third option", q12a)
+
+	//
+	q10b.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q10b.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q10b.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q9.AddOption("first option", q10b)
+
+	q11b.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q11b.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q11b.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q9.AddOption("second option", q11b)
+
+	q12b.AddConfig("first option", models.NewConfigOption("myconf", nil))
+	q12b.AddConfig("second option", models.NewConfigOption("myconf", nil))
+	q12b.AddConfig("third option", models.NewConfigOption("myconf", nil))
+	q9.AddOption("third option", q12b)
+
+	return *q1
 }
 
 // Configs ...
