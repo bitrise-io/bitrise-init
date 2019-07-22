c4e48a33d877f00758b14027e3d9d71aa5721414
Richard Bogdan <richard.bogdan@bitrise.io>
2019-07-22 09:57:34 +0200

    TOOL-134 Added stack to bitrise-init.

---
+++ stack/stack.go
@@ -0,0 +1,28 @@
+package stack
+
+var DefaultStacks = map[string]string{
+	"xamarin":      "osx-vs4mac-stable",
+	"cordova":      "osx-vs4mac-stable",
+	"react-native": "osx-vs4mac-stable",
+	"ionic":        "osx-vs4mac-stable",
+	"flutter":      "osx-vs4mac-stable",
+	"android":      "linux-docker-android",
+	"macos":        "osx-xcode-10.0.x",
+	"ios":          "osx-xcode-10.0.x",
+}
+
+var optionsStacks = []string{
+	"linux-docker-android-lts",
+	"linux-docker-android",
+	"osx-vs4mac-beta",
+	"osx-vs4mac-previous-stable",
+	"osx-vs4mac-stable",
+	"osx-xamarin-stable",
+	"osx-xcode-10.0.x",
+	"osx-xcode-10.1.x",
+	"osx-xcode-10.2.x",
+	"osx-xcode-8.3.x",
+	"osx-xcode-9.2.x",
+	"osx-xcode-9.4.x",
+	"osx-xcode-edge",
+}
