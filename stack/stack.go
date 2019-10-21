40062d89383e6b2070d154b5243cff806e62260a
Richard Bogdan <richard.bogdan@bitrise.io>
2019-10-21 15:34:50 +0200

    TOOL-134 Refactoring type names.

--- stack/stack.go
+++ stack/stack.go
@@ -40,7 +40,9 @@ var optionsStacks = []string{
 }
 
 // StackOptionsMap holds the available stack for the given platforms, where the first item is the default.
-var StackOptionsMap = map[PLATFORM][]STACK{
+//
+// This should be in sync with: https://github.com/bitrise-io/bitrise-website/blob/master/config/available_stacks.yml
+var StackOptionsMap = map[Platform][]Stack{
 	XAMARIN: {
 		OSX_VS4MAC_STABLE,
 		OSX_VS4MAC_BETA,
@@ -151,35 +153,35 @@ var StackOptionsMap = map[PLATFORM][]STACK{
 		OSX_XCODE_EDGE},
 }
 
-// STACK defines a Stack with it's name.
-type STACK string
+// Stack defines a Stack with it's name.
+type Stack string
 
 const (
-	LINUX_DOCKER_ANDROID_LTS   STACK = "linux-docker-android-lts"
-	LINUX_DOCKER_ANDROID       STACK = "linux-docker-android"
-	OSX_VS4MAC_BETA            STACK = "osx-vs4mac-beta"
-	OSX_VS4MAC_PREVIOUS_STABLE STACK = "osx-vs4mac-previous-stable"
-	OSX_VS4MAC_STABLE          STACK = "osx-vs4mac-stable"
-	OSX_XCODE_11_2_X           STACK = "osx-xcode-11.2.x"
-	OSX_XCODE_11_1_X           STACK = "osx-xcode-11.1.x"
-	OSX_XCODE_11_0_X           STACK = "osx-xcode-11.0.x"
-	OSX_XCODE_10_2_X           STACK = "osx-xcode-10.2.x"
-	OSX_XCODE_10_1_X           STACK = "osx-xcode-10.1.x"
-	OSX_XCODE_10_0_X           STACK = "osx-xcode-10.0.x"
-	OSX_XCODE_9_4_X            STACK = "osx-xcode-9.4.x"
-	OSX_XCODE_8_3_X            STACK = "osx-xcode-8.3.x"
-	OSX_XCODE_EDGE             STACK = "osx-xcode-edge"
+	LINUX_DOCKER_ANDROID_LTS   Stack = "linux-docker-android-lts"
+	LINUX_DOCKER_ANDROID       Stack = "linux-docker-android"
+	OSX_VS4MAC_BETA            Stack = "osx-vs4mac-beta"
+	OSX_VS4MAC_PREVIOUS_STABLE Stack = "osx-vs4mac-previous-stable"
+	OSX_VS4MAC_STABLE          Stack = "osx-vs4mac-stable"
+	OSX_XCODE_11_2_X           Stack = "osx-xcode-11.2.x"
+	OSX_XCODE_11_1_X           Stack = "osx-xcode-11.1.x"
+	OSX_XCODE_11_0_X           Stack = "osx-xcode-11.0.x"
+	OSX_XCODE_10_2_X           Stack = "osx-xcode-10.2.x"
+	OSX_XCODE_10_1_X           Stack = "osx-xcode-10.1.x"
+	OSX_XCODE_10_0_X           Stack = "osx-xcode-10.0.x"
+	OSX_XCODE_9_4_X            Stack = "osx-xcode-9.4.x"
+	OSX_XCODE_8_3_X            Stack = "osx-xcode-8.3.x"
+	OSX_XCODE_EDGE             Stack = "osx-xcode-edge"
 )
 
 // Stacks is the array of the all available stacks.
-var Stacks = []STACK{
+var Stacks = []Stack{
 	LINUX_DOCKER_ANDROID_LTS, LINUX_DOCKER_ANDROID, OSX_VS4MAC_BETA, OSX_VS4MAC_PREVIOUS_STABLE, OSX_XCODE_11_2_X,
 	OSX_XCODE_11_1_X, OSX_XCODE_11_0_X, OSX_XCODE_10_2_X, OSX_XCODE_10_1_X, OSX_XCODE_10_0_X, OSX_XCODE_9_4_X,
 	OSX_XCODE_8_3_X, OSX_XCODE_EDGE,
 }
 
 // ParseStack gets the given stack from a string.
-func ParseStack(s string) (STACK, error) {
+func ParseStack(s string) (Stack, error) {
 	for _, stack := range Stacks {
 		if string(stack) == s {
 			return stack, nil
@@ -188,32 +190,32 @@ func ParseStack(s string) (STACK, error) {
 	return "", fmt.Errorf("could not find stack %s", s)
 }
 
-// StringValue returns the string value of the given STACK.
-func (s STACK) StringValue() string {
+// StringValue returns the string value of the given Stack.
+func (s Stack) StringValue() string {
 	return string(s)
 }
 
-// PLATFORM defines a platform with it's name.
-type PLATFORM string
+// Platform defines a platform with it's name.
+type Platform string
 
 const (
-	XAMARIN      PLATFORM = "xamarin"
-	CORDOVA      PLATFORM = "cordova"
-	REACT_NATIVE PLATFORM = "react-native"
-	IONIC        PLATFORM = "ionic"
-	FLUTTER      PLATFORM = "flutter"
-	ANDROID      PLATFORM = "android"
-	MACOS        PLATFORM = "macos"
-	IOS          PLATFORM = "ios"
+	XAMARIN      Platform = "xamarin"
+	CORDOVA      Platform = "cordova"
+	REACT_NATIVE Platform = "react-native"
+	IONIC        Platform = "ionic"
+	FLUTTER      Platform = "flutter"
+	ANDROID      Platform = "android"
+	MACOS        Platform = "macos"
+	IOS          Platform = "ios"
 )
 
-// platforms is the array of all available platforms.
-var Platforms = []PLATFORM{
+// Platforms is the array of all available platforms.
+var Platforms = []Platform{
 	XAMARIN, CORDOVA, REACT_NATIVE, IONIC, FLUTTER, ANDROID, MACOS, IOS,
 }
 
 // ParsePlatform gets the given platform from a string.
-func ParsePlatform(s string) (PLATFORM, error) {
+func ParsePlatform(s string) (Platform, error) {
 	for _, platform := range Platforms {
 		if string(platform) == s {
 			return platform, nil
@@ -222,7 +224,7 @@ func ParsePlatform(s string) (PLATFORM, error) {
 	return "", fmt.Errorf("could not find platorm %s", s)
 }
 
-// StringValue returns the string value of the given PLATFORM.
-func (p PLATFORM) StringValue() string {
+// StringValue returns the string value of the given Platform.
+func (p Platform) StringValue() string {
 	return string(p)
 }
