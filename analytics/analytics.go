a8f36859c5f4e2b3e01b32eeaba97bedafce19e2
Krisztián Gödrei <godreikrisztian@gmail.com>
2020-09-15 16:16:03 +0200

    Send analytics log if scanner fails. (#179)
    * Send analytics log if scanner fails.
    
    * Dependencies update.
    
    * Fix missing ... in args forward.
    
    * Send error and detector name in data, use log_level error.
    
    * Update stacks test.
    
    * Unify detector erro data creation.
    
    * Revert "Dependencies update."
    
    This reverts commit 5b4178ddbc5b6dbf259ae57075339543d800cfaf.
    
    * Remove build_slug from analytics data.
    
    As BITRISE_BUILD_SLUG env is not available during add-new-project builds.
    
    * Update LogError function comment.

---
+++ analytics/analytics.go
@@ -0,0 +1,10 @@
+package analytics
+
+import (
+	"github.com/bitrise-io/go-utils/log"
+)
+
+// LogError sends analytics log using log.RErrorf by setting the stepID.
+func LogError(tag string, data map[string]interface{}, format string, v ...interface{}) {
+	log.RErrorf("bitrise-init", tag, data, format, v...)
+}
