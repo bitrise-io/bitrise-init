92a7576466e241195cbb4adc79b985502bc8289d
doomsayer13 <doomsayer13@gmail.com>
2020-10-15 16:09:58 +0200

    Add new step error type

--- step/steperror.go
+++ step/steperror.go
@@ -4,6 +4,22 @@ package step
 type Error struct {
 	StepID, Tag, ShortMsg string
 	Err                   error
+	Recommendations       Recommendation
+}
+
+// BranchReccomendations ...
+type BranchReccomendations struct {
+	AvailableBranches []string
+}
+
+// Recommendation interface
+type Recommendation interface {
+	GetRecommendationID() string
+}
+
+// GetRecommendationID ...
+func (br BranchReccomendations) GetRecommendationID() string {
+	return "branchRecommendation"
 }
 
 // NewError constructs a step.Error
@@ -13,6 +29,17 @@ func NewError(stepID, tag string, err error, shortMsg string) *Error {
 		Tag:      tag,
 		Err:      err,
 		ShortMsg: shortMsg,
+	}
+}
+
+// NewErrorWithRecommendations constructs a step.Error
+func NewErrorWithRecommendations(stepID, tag string, err error, shortMsg string, recommendations Recommendation) *Error {
+	return &Error{
+		StepID:          stepID,
+		Tag:             tag,
+		Err:             err,
+		ShortMsg:        shortMsg,
+		Recommendations: recommendations,
 	}
 }
 
