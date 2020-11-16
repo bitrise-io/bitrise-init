14631ad4e62276675c467770dceed8d02b921aa6
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-11-16 15:52:19 +0100

    Ignoring Android scanner errors resulting from icon lookup failures.
    - If icon format is unsupported, does not fail, instead returns default icons.
    - Send failures to analytics Info level log, does not return warnings to user

---
+++ analytics/analytics_test.go
@@ -0,0 +1,48 @@
+package analytics
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_initData(t *testing.T) {
+	type args struct {
+	}
+	tests := []struct {
+		name       string
+		data, want map[string]interface{}
+	}{
+		{
+			name: "Empty data",
+			data: map[string]interface{}{},
+			want: map[string]interface{}{
+				"source": "scanner",
+			},
+		},
+		{
+			name: "nil data",
+			data: nil,
+			want: map[string]interface{}{
+				"source": "scanner",
+			},
+		},
+		{
+			name: "Existing data",
+			data: map[string]interface{}{
+				"A": "B",
+			},
+			want: map[string]interface{}{
+				"source": "scanner",
+				"A":      "B",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if got := initData(tt.data); !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("initData() = %v, want %s", got, tt.want)
+			}
+		})
+	}
+}
