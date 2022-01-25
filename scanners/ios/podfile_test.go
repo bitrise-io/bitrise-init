6682bda67ae69db515da294668ac1a244919001e
lpusok <7979773+lpusok@users.noreply.github.com>
2022-01-25 15:12:16 +0100

    Update Android deploy WF (#219)
    *  Introduces the isPrivateRepo paramter in the Configs method of ScannerInterface. This can be used to omit Activate SSH Step.
    *  Fixed an unrelated Cocoapods test.

--- scanners/ios/podfile_test.go
+++ scanners/ios/podfile_test.go
@@ -107,7 +107,7 @@ pod 'Alamofire', '~> 3.4'
 		require.Equal(t, expectedTargetDefinition, actualTargetDefinition)
 	}
 
-	t.Log("cocoapods 0.38.0")
+	t.Log("cocoapods 1.8.4")
 	{
 		tmpDir = filepath.Join(tmpDir, "xcodeproj_not_defined")
 		require.NoError(t, os.MkdirAll(tmpDir, 0777))
@@ -117,8 +117,8 @@ platform :ios, '8.0'
 
 # pod 'Functional.m', '~> 1.0'
 
-# Add Kiwi as an exclusive dependency for the Test target
-target :SampleAppWithCocoapodsTests, :exclusive => true do
+# Add Kiwi as a dependency for the Test target
+target :SampleAppWithCocoapodsTests do
   pod 'Kiwi'
 end
 
@@ -134,7 +134,7 @@ end
 		podparser := podfileParser{podfilePth: podfilePth}
 
 		expectedTargetDefinition := map[string]string{}
-		actualTargetDefinition, err := podparser.getTargetDefinitionProjectMap("0.38.0")
+		actualTargetDefinition, err := podparser.getTargetDefinitionProjectMap("1.8.4")
 		require.NoError(t, err)
 		require.Equal(t, expectedTargetDefinition, actualTargetDefinition)
 	}
