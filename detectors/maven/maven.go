9d4b925971a3970544c7c5be3ede52704038daf0
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-04-08 15:38:15 +0200

    Move direntry to detectors package and impleemnt basic Maven detector

---
+++ detectors/maven/maven.go
@@ -0,0 +1,37 @@
+package maven
+
+import (
+	"fmt"
+
+	"github.com/bitrise-io/bitrise-init/detectors/direntry"
+)
+
+type Project struct {
+	RootDirEntry                direntry.DirEntry
+	ProjectObjectModelFileEntry direntry.DirEntry
+}
+
+func ScanProject(searchDir string) (*Project, error) {
+	rootEntry, err := direntry.WalkDir(searchDir, 6)
+	if err != nil {
+		return nil, err
+	}
+	return detectMavenProjectRoot(*rootEntry)
+}
+
+func detectMavenProjectRoot(searchDir direntry.DirEntry) (*Project, error) {
+	projectObjectModelEntry := searchDir.FindFirstEntryByName("pom.xml", false)
+	if projectObjectModelEntry == nil {
+		return nil, nil
+	}
+
+	projectRootDirEntry := projectObjectModelEntry.Parent()
+	if projectRootDirEntry == nil {
+		return nil, fmt.Errorf("unable to detect project root")
+	}
+
+	return &Project{
+		RootDirEntry:                *projectRootDirEntry,
+		ProjectObjectModelFileEntry: *projectObjectModelEntry,
+	}, nil
+}
