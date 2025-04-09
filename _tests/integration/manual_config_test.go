bcdd062171e8f09f0c10eebf7c6ace6a329559f9
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-04-09 09:06:18 +0200

    Kotlin Multiplatform scanner (#283)
    * Kotlin Multiplatform scanner init
    
    * Detect kmp project structure
    
    * Generic Gradle scanner
    
    * Wire in Gradle scanner to the KMP scanner
    
    * Refactor gradle dependency detection
    
    * Sort gradle projects
    
    * Kotlin Multiplatform test workflow
    
    * Kotlin Multiplatform scanner implementation
    
    * Improve Kotlin Multplatform logging and config test
    
    * Update manual config tests
    
    * Fix master merge
    
    * Fix lint issues
    
    * Update tests
    
    * Use Gradle Uni Test step in KMP scanner
    
    * Update Kotlin Multiplatform sample app url
    
    * Move direntry package under gradle detector package and document direntry functions
    
    * delegate os.ReadDir error to the caller
    
    * Rename FindDirectEntryByName to FindImmediateChildByName and fix Gradle project root detection
    
    * Increase dir walk depth

--- _tests/integration/manual_config_test.go
+++ _tests/integration/manual_config_test.go
@@ -226,6 +226,13 @@ var customConfigVersions = []interface{}{
 	steps.PullIntermediateFilesVersion,
 	steps.XcodeTestWithoutBuildingVersion,
 
+	// kotlin-multiplatform
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.GradleUnitTestVersion,
+	steps.DeployToBitriseIoVersion,
+
 	// macos
 	models.FormatVersion,
 	steps.ActivateSSHKeyVersion,
@@ -469,6 +476,14 @@ var customConfigResultYML = fmt.Sprintf(`options:
                 config: default-ios-config
               enterprise:
                 config: default-ios-config
+  kotlin-multiplatform:
+    title: The project's Gradle Wrapper script (gradlew) path.
+    summary: The project's Gradle Wrapper script (gradlew) path.
+    env_key: GRADLEW_PATH
+    type: user_input
+    value_map:
+      "":
+        config: default-kotlin-multiplatform-config
   macos:
     title: Project or Workspace path
     summary: The location of your Xcode project, Xcode workspace or SPM project files
@@ -1150,6 +1165,21 @@ configs:
               inputs:
               - only_testing: $BITRISE_TEST_SHARDS_PATH/$BITRISE_IO_PARALLEL_INDEX
               - xctestrun: $BITRISE_TEST_BUNDLE_PATH/all_tests.xctestrun
+  kotlin-multiplatform:
+    default-kotlin-multiplatform-config: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: kotlin-multiplatform
+      workflows:
+        run_tests:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - gradle-unit-test@%s:
+              inputs:
+              - gradlew_path: $GRADLEW_PATH
+          - deploy-to-bitrise-io@%s: {}
   macos:
     default-macos-config: |
       format_version: "%s"
