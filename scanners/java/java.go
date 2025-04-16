1b5834373930c88174ee5fec314b8c244f2a8f4a
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-04-16 13:56:43 +0200

    Detec Gradle and Maven based Java projects and implement options

--- scanners/java/java.go
+++ scanners/java/java.go
@@ -2,19 +2,37 @@ package java
 
 import (
 	"github.com/bitrise-io/bitrise-init/detectors/gradle"
+	"github.com/bitrise-io/bitrise-init/detectors/maven"
 	"github.com/bitrise-io/bitrise-init/models"
 	"github.com/bitrise-io/go-utils/log"
 )
 
 const (
-	projectType       = "java"
-	configName        = "java-config"
-	defaultConfigName = "default-java-config"
-	testWorkflowID    = "run_tests"
+	projectType = "java"
+
+	buildToolInputTitle   = "Build tool"
+	buildToolInputSummary = "The build tool used in the project. Supported options: Gradle, Maven."
+	buildToolGradle       = "Gradle"
+	buildToolMaven        = "Maven"
+
+	testWorkflowID = "run_tests"
+
+	gradleConfigName        = "java-gradle-config"
+	defaultGradleConfigName = "default-java-gradle-config"
+	gradlewPathInputEnvKey  = "GRADLEW_PATH"
+	gradlewPathInputTitle   = "The project's Gradle Wrapper script (gradlew) path."
+	gradlewPathInputSummary = "The project's Gradle Wrapper script (gradlew) path."
+
+	mavenConfigName                 = "java-maven-config"
+	defaultMavenConfigName          = "default-java-maven-config"
+	mavenProjectRootDirInputEnvKey  = "MAVEN_PROJECT_ROOT_DIR"
+	mavenProjectRootDirInputTitle   = "The root directory of the Maven project (where the pom.xml file is located)."
+	mavenProjectRootDirInputSummary = "The root directory of the Maven project (where the pom.xml file is located)."
 )
 
 type Scanner struct {
-	gradleProject gradle.Project
+	gradleProject *gradle.Project
+	mavenProject  *maven.Project
 }
 
 func NewScanner() *Scanner {
@@ -23,6 +41,87 @@ func NewScanner() *Scanner {
 
 func (s *Scanner) Name() string {
 	return projectType
+}
+
+func (s *Scanner) DetectPlatform(searchDir string) (bool, error) {
+	log.TInfof("Searching for Gradle project files...")
+
+	gradleProject, err := gradle.ScanProject(searchDir)
+	if err != nil {
+		return false, err
+	}
+
+	log.TDonef("Gradle project found: %v", gradleProject != nil)
+	if gradleProject != nil {
+		s.gradleProject = gradleProject
+		printGradleProject(*gradleProject)
+		return true, nil
+	}
+
+	log.TInfof("Searching for Maven project files...")
+	mavenProject, err := maven.ScanProject(searchDir)
+	if err != nil {
+		return false, err
+	}
+
+	log.TDonef("Maven project found: %v", mavenProject != nil)
+	if mavenProject != nil {
+		s.mavenProject = mavenProject
+		printMavenProject(*mavenProject)
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func (s *Scanner) ExcludedScannerNames() []string {
+	return []string{}
+}
+
+func (s *Scanner) Options() (models.OptionNode, models.Warnings, models.Icons, error) {
+	if s.gradleProject != nil {
+		gradlewPathOption := models.NewOption(gradlewPathInputTitle, gradlewPathInputSummary, gradlewPathInputEnvKey, models.TypeSelector)
+		configOption := models.NewConfigOption(gradleConfigName, nil)
+		gradlewPathOption.AddConfig(s.gradleProject.GradlewFileEntry.RelPath, configOption)
+		return *gradlewPathOption, nil, nil, nil
+	}
+
+	if s.mavenProject != nil {
+		mavenProjectRootDirOption := models.NewOption(mavenProjectRootDirInputTitle, mavenProjectRootDirInputSummary, mavenProjectRootDirInputEnvKey, models.TypeSelector)
+		configOption := models.NewConfigOption(mavenConfigName, nil)
+		mavenProjectRootDirOption.AddConfig(s.mavenProject.RootDirEntry.RelPath, configOption)
+		return *mavenProjectRootDirOption, nil, nil, nil
+	}
+
+	return models.OptionNode{}, nil, nil, nil
+}
+
+func (s *Scanner) DefaultOptions() models.OptionNode {
+	buildToolOption := models.NewOption(buildToolInputTitle, buildToolInputSummary, "", models.TypeSelector)
+
+	gradlewPathOption := models.NewOption(gradlewPathInputTitle, gradlewPathInputSummary, gradlewPathInputEnvKey, models.TypeUserInput)
+	buildToolOption.AddOption(buildToolGradle, gradlewPathOption)
+
+	gradleConfigOption := models.NewConfigOption(defaultGradleConfigName, nil)
+	gradlewPathOption.AddConfig("", gradleConfigOption)
+
+	mavenProjectRootDirOption := models.NewOption(mavenProjectRootDirInputTitle, mavenProjectRootDirInputSummary, mavenProjectRootDirInputEnvKey, models.TypeUserInput)
+	buildToolOption.AddOption(buildToolMaven, mavenProjectRootDirOption)
+
+	mavenConfigOption := models.NewConfigOption(defaultMavenConfigName, nil)
+	mavenProjectRootDirOption.AddConfig("", mavenConfigOption)
+
+	return *buildToolOption
+}
+
+func (s *Scanner) Configs(sshKeyActivation models.SSHKeyActivation) (models.BitriseConfigMap, error) {
+	// TODO: implement
+	return models.BitriseConfigMap{}, nil
+}
+
+func (s *Scanner) DefaultConfigs() (models.BitriseConfigMap, error) {
+	// TODO: implement
+	return models.BitriseConfigMap{}, nil
 }
 
 func printGradleProject(gradleProject gradle.Project) {
@@ -45,46 +144,7 @@ func printGradleProject(gradleProject gradle.Project) {
 	}
 }
 
-func (s *Scanner) DetectPlatform(searchDir string) (bool, error) {
-	log.TInfof("Searching for Gradle project files...")
-
-	gradleProject, err := gradle.ScanProject(searchDir)
-	if err != nil {
-		return false, err
-	}
-
-	log.TDonef("Gradle project found: %v", gradleProject != nil)
-	if gradleProject != nil {
-		printGradleProject(*gradleProject)
-	} else {
-
-	}
-
-	// TODO: implement
-	return false, nil
-}
-
-func (s *Scanner) ExcludedScannerNames() []string {
-	// TODO: implement
-	return []string{}
-}
-
-func (s *Scanner) Options() (models.OptionNode, models.Warnings, models.Icons, error) {
-	// TODO: implement
-	return models.OptionNode{}, nil, nil, nil
-}
-
-func (s *Scanner) DefaultOptions() models.OptionNode {
-	// TODO: implement
-	return models.OptionNode{}
-}
-
-func (s *Scanner) Configs(sshKeyActivation models.SSHKeyActivation) (models.BitriseConfigMap, error) {
-	// TODO: implement
-	return models.BitriseConfigMap{}, nil
-}
-
-func (s *Scanner) DefaultConfigs() (models.BitriseConfigMap, error) {
-	// TODO: implement
-	return models.BitriseConfigMap{}, nil
+func printMavenProject(mavenProject maven.Project) {
+	log.TPrintf("Project root dir: %s", mavenProject.RootDirEntry.RelPath)
+	log.TPrintf("Maven POM file: %s", mavenProject.ProjectObjectModelFileEntry.RelPath)
 }
