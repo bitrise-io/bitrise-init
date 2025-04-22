6329146206a1c3b0c1ada495c3b1c6c2b61ff89b
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-04-22 10:59:02 +0200

    Code cleanup

--- scanners/android/android_test.go
+++ scanners/android/android_test.go
@@ -6,44 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: restore tests
-//import (
-//	"fmt"
-//	"path/filepath"
-//	"testing"
-//
-//	"github.com/bitrise-io/bitrise-init/models"
-//	"github.com/bitrise-io/go-utils/command/git"
-//	"github.com/stretchr/testify/require"
-//)
-//
-//func gitClone(t *testing.T, dir, uri string) {
-//	fmt.Printf("cloning into: %s\n", dir)
-//	g, err := git.New(dir)
-//	require.NoError(t, err)
-//	require.NoError(t, g.Clone(uri).Run())
-//}
-//
-//func Test_detect(t *testing.T) {
-//	t.Run("Sample app", func(t *testing.T) {
-//		sampleAppDir := t.TempDir()
-//		sampleAppURL := "https://github.com/bitrise-samples/sample-apps-android-sdk22.git"
-//		gitClone(t, sampleAppDir, sampleAppURL)
-//
-//		want := []Project{{
-//			RelPath: ".",
-//			Icons: models.Icons{{
-//				Filename: "81af22c35b03b30a1931a6283349eae094463aa69c52af3afe804b40dbe6dc12.png",
-//				Path:     filepath.Join(sampleAppDir, "app", "src", "main", "res", "mipmap-xxxhdpi", "ic_launcher.png"),
-//			}},
-//		}}
-//
-//		got, err := detect(sampleAppDir)
-//		require.NoError(t, err)
-//		require.Equal(t, want, got)
-//	})
-//}
-
 func Test_modulePathFromBuildScriptPath(t *testing.T) {
 	tests := []struct {
 		name           string
