7e92bc50643b63b3b5c6704fcaf92ed244284bf2
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-09-01 10:10:06 +0200

    Update kmp scanner integration test

--- _tests/integration/kmp_test.go
+++ _tests/integration/kmp_test.go
@@ -33,21 +33,101 @@ var kmpTaskmanResultYaml = fmt.Sprintf(`options:
     type: selector
     value_map:
       ./:
-        config: kotlin-multiplatform-config
+        title: Module
+        summary: Modules provide a container for your Android project's source code,
+          resource files, and app level settings, such as the module-level build file
+          and Android manifest file. Each module can be independently built, tested,
+          and debugged. You can add new modules to your Bitrise builds at any time.
+        env_key: MODULE
+        type: user_input
+        value_map:
+          composeApp:
+            title: Variant
+            summary: Your Android build variant. You can add variants at any time,
+              as well as further configure your existing variants later.
+            env_key: VARIANT
+            type: user_input_optional
+            value_map:
+              "":
+                title: Project or Workspace path
+                summary: The location of your Xcode project, Xcode workspace or SPM
+                  project files stored as an Environment Variable. In your Workflows,
+                  you can specify paths relative to this path.
+                env_key: BITRISE_PROJECT_PATH
+                type: selector
+                value_map:
+                  ./iosApp/iosApp.xcodeproj:
+                    title: Scheme name
+                    summary: An Xcode scheme defines a collection of targets to build,
+                      a configuration to use when building, and a collection of tests
+                      to execute. Only shared schemes are detected automatically but
+                      you can use any scheme as a target on Bitrise. You can change
+                      the scheme at any time in your Env Vars.
+                    env_key: BITRISE_SCHEME
+                    type: selector
+                    value_map:
+                      iosApp:
+                        title: Distribution method
+                        summary: The export method used to create an .ipa file in
+                          your builds, stored as an Environment Variable. You can
+                          change this at any time, or even create several .ipa files
+                          with different export methods in the same build.
+                        env_key: BITRISE_DISTRIBUTION_METHOD
+                        type: selector
+                        value_map:
+                          ad-hoc:
+                            config: kotlin-multiplatform-config
+                          app-store:
+                            config: kotlin-multiplatform-config
+                          development:
+                            config: kotlin-multiplatform-config
+                          enterprise:
+                            config: kotlin-multiplatform-config
 configs:
   kotlin-multiplatform:
     kotlin-multiplatform-config: |
       format_version: "%s"
       default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
       project_type: kotlin-multiplatform
+      pipelines:
+        build:
+          workflows:
+            android_build: {}
+            ios_build: {}
       workflows:
+        android_build:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - android-build@%s:
+              inputs:
+              - project_location: $PROJECT_ROOT_DIR
+              - module: $MODULE
+              - variant: $VARIANT
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+        ios_build:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - xcode-archive@%s:
+              inputs:
+              - project_path: $BITRISE_PROJECT_PATH
+              - scheme: $BITRISE_SCHEME
+              - distribution_method: $BITRISE_DISTRIBUTION_METHOD
+              - configuration: Release
+              - automatic_code_signing: api-key
+          - deploy-to-bitrise-io@%s: {}
         run_tests:
           steps:
           - activate-ssh-key@%s: {}
           - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
           - gradle-unit-test@%s:
               inputs:
               - project_root_dir: $PROJECT_ROOT_DIR
+          - save-gradle-cache@%s: {}
           - deploy-to-bitrise-io@%s: {}
 warnings:
   kotlin-multiplatform: []
@@ -57,8 +137,23 @@ warnings_with_recommendations:
 
 var kmpTaskmanResultVersions = []interface{}{
 	models.FormatVersion,
+	// android_build
 	steps.ActivateSSHKeyVersion,
 	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.AndroidBuildVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	// ios_build
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.XcodeArchiveVersion,
+	steps.DeployToBitriseIoVersion,
+	// run_tests
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
 	steps.GradleUnitTestVersion,
+	steps.CacheSaveGradleVersion,
 	steps.DeployToBitriseIoVersion,
 }
