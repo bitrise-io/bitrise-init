05c0f54c474aede0f9b68285f1ac437acec7eeaf
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-09-08 09:27:40 +0200

    Update kmp scanner (#292)
    * Create Kotlin Multiplatform detector
    
    * Distinguish Android App and Wear targets
    
    * Wire in native ios and android scanners for the kmp scanners
    
    * Wire in ios and android build workflows and pipeline with cache
    
    * Add KMP manual options and configs
    
    * Update kmp scanner integration test
    
    * Android app module input type is selector
    
    * Fix kmp scanner nil pointer issue
    
    * Fix kmp scanner manual config tests
    
    * Add sign-apk to kmp android workflows
    
    * Add Activate Build Cache For Gradle step to KMP workflows and update step versions
    
    * Update kmp scanner option titles and summaries
    
    * Code cleanup
    
    * Rename SubProject receiver
    
    * Continue kmp scanner if native targets scanning fails
    
    * Rename GetDependencyID to GetPluginAliasFromVersionCatalog
    
    * Update detectors/kmp/kmp.go
    
    Co-authored-by: Olivér Falvai <oliver.falvai@bitrise.io>
    
    * Update plugin alias - accessor conversion
    
    ---------
    
    Co-authored-by: Olivér Falvai <oliver.falvai@bitrise.io>

--- _tests/integration/kmp_test.go
+++ _tests/integration/kmp_test.go
@@ -25,29 +25,103 @@ func TestKotlinMultiplatform(t *testing.T) {
 
 var kmpTaskmanResultYaml = fmt.Sprintf(`options:
   kotlin-multiplatform:
-    title: The root directory of the Gradle project.
-    summary: The root directory of the Gradle project, which contains all source files
-      from your project, as well as Gradle files, including the Gradle Wrapper (`+"`gradlew`"+`)
-      file.
+    title: The root directory of the Kotlin Multiplatform project.
+    summary: The root directory of the Kotlin Multiplatform project, which contains
+      all source files from your project, as well as Gradle files, including the Gradle
+      Wrapper (gradlew) file.
     env_key: PROJECT_ROOT_DIR
     type: selector
     value_map:
       ./:
-        config: kotlin-multiplatform-config
+        title: Android Application Module
+        summary: The name of the Android application module to build.
+        env_key: MODULE
+        type: selector
+        value_map:
+          composeApp:
+            title: Android Application Variant
+            summary: The name of the Android application variant to build.
+            env_key: VARIANT
+            type: user_input_optional
+            value_map:
+              "":
+                title: iOS Application Project or Workspace path
+                summary: The path of iOS application Xcode project or workspace to
+                  build.
+                env_key: BITRISE_PROJECT_PATH
+                type: selector
+                value_map:
+                  ./iosApp/iosApp.xcodeproj:
+                    title: iOS Application Scheme
+                    summary: The name of the iOS application scheme to build.
+                    env_key: BITRISE_SCHEME
+                    type: selector
+                    value_map:
+                      iosApp:
+                        title: iOS Application Distribution method
+                        summary: The export method to use to build the iOS application
+                          IPA file.
+                        env_key: BITRISE_DISTRIBUTION_METHOD
+                        type: selector
+                        value_map:
+                          ad-hoc:
+                            config: kotlin-multiplatform-config
+                          app-store:
+                            config: kotlin-multiplatform-config
+                          development:
+                            config: kotlin-multiplatform-config
+                          enterprise:
+                            config: kotlin-multiplatform-config
 configs:
   kotlin-multiplatform:
     kotlin-multiplatform-config: |
       format_version: "%s"
       default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
       project_type: kotlin-multiplatform
+      pipelines:
+        build:
+          workflows:
+            android_build: {}
+            ios_build: {}
       workflows:
+        android_build:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - android-build@%s:
+              inputs:
+              - project_location: $PROJECT_ROOT_DIR
+              - module: $MODULE
+              - variant: $VARIANT
+          - sign-apk@%s:
+              run_if: '{{getenv "BITRISEIO_ANDROID_KEYSTORE_URL" | ne ""}}'
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+        ios_build:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - xcode-archive@%s:
+              inputs:
+              - project_path: $BITRISE_PROJECT_PATH
+              - scheme: $BITRISE_SCHEME
+              - distribution_method: $BITRISE_DISTRIBUTION_METHOD
+              - configuration: Release
+              - automatic_code_signing: api-key
+          - deploy-to-bitrise-io@%s: {}
         run_tests:
           steps:
           - activate-ssh-key@%s: {}
           - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
           - gradle-unit-test@%s:
               inputs:
               - project_root_dir: $PROJECT_ROOT_DIR
+          - save-gradle-cache@%s: {}
           - deploy-to-bitrise-io@%s: {}
 warnings:
   kotlin-multiplatform: []
@@ -57,8 +131,27 @@ warnings_with_recommendations:
 
 var kmpTaskmanResultVersions = []interface{}{
 	models.FormatVersion,
+	// android_build
 	steps.ActivateSSHKeyVersion,
 	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.AndroidBuildVersion,
+	steps.SignAPKVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	// ios_build
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.XcodeArchiveVersion,
+	steps.DeployToBitriseIoVersion,
+	// run_tests
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
 	steps.GradleUnitTestVersion,
+	steps.CacheSaveGradleVersion,
 	steps.DeployToBitriseIoVersion,
 }
--- _tests/integration/manual_config_test.go
+++ _tests/integration/manual_config_test.go
@@ -241,10 +241,67 @@ var customConfigVersions = []interface{}{
 	steps.DeployToBitriseIoVersion,
 
 	// kotlin-multiplatform
+	// default-kotlin-multiplatform-config
 	models.FormatVersion,
 	steps.ActivateSSHKeyVersion,
 	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
 	steps.GradleUnitTestVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	// default-kotlin-multiplatform-config-android
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.AndroidBuildVersion,
+	steps.SignAPKVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.GradleUnitTestVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	// default-kotlin-multiplatform-config-android-ios
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.AndroidBuildVersion,
+	steps.SignAPKVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.XcodeArchiveVersion,
+	steps.DeployToBitriseIoVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.GradleUnitTestVersion,
+	steps.CacheSaveGradleVersion,
+	steps.DeployToBitriseIoVersion,
+	// default-kotlin-multiplatform-config-ios
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.XcodeArchiveVersion,
+	steps.DeployToBitriseIoVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreGradleVersion,
+	steps.ActivateBuildCacheForGradleVersion,
+	steps.GradleUnitTestVersion,
+	steps.CacheSaveGradleVersion,
 	steps.DeployToBitriseIoVersion,
 
 	// macos
@@ -516,15 +573,101 @@ var customConfigResultYML = fmt.Sprintf(`options:
           "":
             config: default-java-maven-config
   kotlin-multiplatform:
-    title: The root directory of the Gradle project.
-    summary: The root directory of the Gradle project, which contains all source files
-      from your project, as well as Gradle files, including the Gradle Wrapper (`+"`gradlew`"+`)
-      file.
+    title: The root directory of the Kotlin Multiplatform project.
+    summary: The root directory of the Kotlin Multiplatform project, which contains
+      all source files from your project, as well as Gradle files, including the Gradle
+      Wrapper (gradlew) file.
     env_key: PROJECT_ROOT_DIR
     type: user_input
     value_map:
       "":
-        config: default-kotlin-multiplatform-config
+        title: Has Android Application target?
+        summary: Indicates whether the project contains an Android Application target.
+        type: selector
+        value_map:
+          "no":
+            title: Has iOS Application target?
+            summary: Indicates whether the project contains an iOS Application target.
+            type: selector
+            value_map:
+              "no":
+                config: default-kotlin-multiplatform-config
+              "yes":
+                title: iOS Application Project or Workspace path
+                summary: The path of iOS application Xcode project or workspace to
+                  build.
+                env_key: BITRISE_PROJECT_PATH
+                type: user_input
+                value_map:
+                  "":
+                    title: iOS Application Scheme
+                    summary: The name of the iOS application scheme to build.
+                    env_key: BITRISE_SCHEME
+                    type: user_input
+                    value_map:
+                      "":
+                        title: iOS Application Distribution method
+                        summary: The export method to use to build the iOS application
+                          IPA file.
+                        env_key: BITRISE_DISTRIBUTION_METHOD
+                        type: selector
+                        value_map:
+                          ad-hoc:
+                            config: default-kotlin-multiplatform-config-ios
+                          app-store:
+                            config: default-kotlin-multiplatform-config-ios
+                          development:
+                            config: default-kotlin-multiplatform-config-ios
+                          enterprise:
+                            config: default-kotlin-multiplatform-config-ios
+          "yes":
+            title: Android Application Module
+            summary: The name of the Android application module to build.
+            env_key: MODULE
+            type: user_input
+            value_map:
+              "":
+                title: Android Application Variant
+                summary: The name of the Android application variant to build.
+                env_key: VARIANT
+                type: user_input_optional
+                value_map:
+                  "":
+                    title: Has iOS Application target?
+                    summary: Indicates whether the project contains an iOS Application
+                      target.
+                    type: selector
+                    value_map:
+                      "no":
+                        config: default-kotlin-multiplatform-config-android
+                      "yes":
+                        title: iOS Application Project or Workspace path
+                        summary: The path of iOS application Xcode project or workspace
+                          to build.
+                        env_key: BITRISE_PROJECT_PATH
+                        type: user_input
+                        value_map:
+                          "":
+                            title: iOS Application Scheme
+                            summary: The name of the iOS application scheme to build.
+                            env_key: BITRISE_SCHEME
+                            type: user_input
+                            value_map:
+                              "":
+                                title: iOS Application Distribution method
+                                summary: The export method to use to build the iOS
+                                  application IPA file.
+                                env_key: BITRISE_DISTRIBUTION_METHOD
+                                type: selector
+                                value_map:
+                                  ad-hoc:
+                                    config: default-kotlin-multiplatform-config-android-ios
+                                  app-store:
+                                    config: default-kotlin-multiplatform-config-android-ios
+                                  development:
+                                    config: default-kotlin-multiplatform-config-android-ios
+                                  enterprise:
+                                    config: default-kotlin-multiplatform-config-android-ios
   macos:
     title: Project or Workspace path
     summary: The location of your Xcode project, Xcode workspace or SPM project files
@@ -1250,9 +1393,123 @@ configs:
           - activate-ssh-key@%s:
               run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
           - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
           - gradle-unit-test@%s:
               inputs:
               - project_root_dir: $PROJECT_ROOT_DIR
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+    default-kotlin-multiplatform-config-android: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: kotlin-multiplatform
+      workflows:
+        android_build:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - android-build@%s:
+              inputs:
+              - project_location: $PROJECT_ROOT_DIR
+              - module: $MODULE
+              - variant: $VARIANT
+          - sign-apk@%s:
+              run_if: '{{getenv "BITRISEIO_ANDROID_KEYSTORE_URL" | ne ""}}'
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+        run_tests:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - gradle-unit-test@%s:
+              inputs:
+              - project_root_dir: $PROJECT_ROOT_DIR
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+    default-kotlin-multiplatform-config-android-ios: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: kotlin-multiplatform
+      workflows:
+        android_build:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - android-build@%s:
+              inputs:
+              - project_location: $PROJECT_ROOT_DIR
+              - module: $MODULE
+              - variant: $VARIANT
+          - sign-apk@%s:
+              run_if: '{{getenv "BITRISEIO_ANDROID_KEYSTORE_URL" | ne ""}}'
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+        ios_build:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - xcode-archive@%s:
+              inputs:
+              - project_path: $BITRISE_PROJECT_PATH
+              - scheme: $BITRISE_SCHEME
+              - distribution_method: $BITRISE_DISTRIBUTION_METHOD
+              - configuration: Release
+              - automatic_code_signing: api-key
+          - deploy-to-bitrise-io@%s: {}
+        run_tests:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - gradle-unit-test@%s:
+              inputs:
+              - project_root_dir: $PROJECT_ROOT_DIR
+          - save-gradle-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+    default-kotlin-multiplatform-config-ios: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: kotlin-multiplatform
+      workflows:
+        ios_build:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - xcode-archive@%s:
+              inputs:
+              - project_path: $BITRISE_PROJECT_PATH
+              - scheme: $BITRISE_SCHEME
+              - distribution_method: $BITRISE_DISTRIBUTION_METHOD
+              - configuration: Release
+              - automatic_code_signing: api-key
+          - deploy-to-bitrise-io@%s: {}
+        run_tests:
+          steps:
+          - activate-ssh-key@%s:
+              run_if: '{{getenv "SSH_RSA_PRIVATE_KEY" | ne ""}}'
+          - git-clone@%s: {}
+          - restore-gradle-cache@%s: {}
+          - activate-build-cache-for-gradle@%s: {}
+          - gradle-unit-test@%s:
+              inputs:
+              - project_root_dir: $PROJECT_ROOT_DIR
+          - save-gradle-cache@%s: {}
           - deploy-to-bitrise-io@%s: {}
   macos:
     default-macos-config: |
