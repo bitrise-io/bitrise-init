12de12821650bffb115523149026cc62d4533b2b
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2026-04-14 13:40:54 +0200

    Add more flutter web tests

--- _tests/integration/flutter_test.go
+++ _tests/integration/flutter_test.go
@@ -38,10 +38,18 @@ func TestFlutter(t *testing.T) {
 		{
 			Name:              "flutter-web",
 			RepoURL:           "https://github.com/bitrise-io/flutter-samples.git",
-			RelativeSearchDir: "flutter-web-fvm",
+			RelativeSearchDir: "flutter-web",
 			Branch:            "main",
 			ExpectedResult:    flutterWebResultYML,
 			ExpectedVersions:  flutterWebVersions,
+		},
+		{
+			Name:              "flutter-ios-android-web",
+			RepoURL:           "https://github.com/bitrise-io/flutter-samples.git",
+			RelativeSearchDir: "flutter-ios-android-web",
+			Branch:            "main",
+			ExpectedResult:    flutterIosAndroidWebResultYML,
+			ExpectedVersions:  flutterIosAndroidWebVersions,
 		},
 	}
 
@@ -413,3 +421,95 @@ warnings:
 warnings_with_recommendations:
   flutter: []
 `, flutterWebVersions...)
+
+var flutterIosAndroidWebVersions = []interface{}{
+	// flutter-config-test-ios-android-0
+	models.FormatVersion,
+	// deploy workflow
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CertificateAndProfileInstallerVersion,
+	steps.FlutterInstallVersion,
+	steps.FlutterAnalyzeVersion,
+	steps.FlutterTestVersion,
+	steps.FlutterBuildVersion,
+	steps.DeployToBitriseIoVersion,
+	// primary workflow
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.FlutterInstallVersion,
+	steps.CacheRestoreDartVersion,
+	steps.FlutterTestVersion,
+	steps.CacheSaveDartVersion,
+	steps.DeployToBitriseIoVersion,
+}
+
+var flutterIosAndroidWebResultYML = fmt.Sprintf(`options:
+  flutter:
+    title: Project location
+    summary: The path to your Flutter project, stored as an Environment Variable.
+      In your Workflows, you can specify paths relative to this path. You can change
+      this at any time.
+    env_key: BITRISE_FLUTTER_PROJECT_LOCATION
+    type: selector
+    value_map:
+      .:
+        config: flutter-config-test-ios-android-web-0
+configs:
+  flutter:
+    flutter-config-test-ios-android-web-0: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: flutter
+      workflows:
+        deploy:
+          description: |
+            Builds and deploys app using [Deploy to bitrise.io Step](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-flutter-projects.html#deploying-a-flutter-app).
+
+            If you build for iOS, make sure to set up code signing secrets on Bitrise for a successful build.
+
+            Next steps:
+            - Check out [Getting started with Flutter apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-flutter-projects.html) for signing and deployment options.
+            - Check out the Code signing guide for [iOS](https://docs.bitrise.io/en/bitrise-ci/code-signing/ios-code-signing.html) and [Android](https://docs.bitrise.io/en/bitrise-ci/code-signing/android-code-signing.html).
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - certificate-and-profile-installer@%s: {}
+          - flutter-installer@%s:
+              inputs:
+              - version: 3.29.2
+          - flutter-analyze@%s:
+              inputs:
+              - project_location: $BITRISE_FLUTTER_PROJECT_LOCATION
+          - flutter-test@%s:
+              inputs:
+              - project_location: $BITRISE_FLUTTER_PROJECT_LOCATION
+          - flutter-build@%s:
+              inputs:
+              - project_location: $BITRISE_FLUTTER_PROJECT_LOCATION
+              - platform: both
+              - ios_output_type: archive
+          - deploy-to-bitrise-io@%s: {}
+        primary:
+          description: |
+            Builds project and runs tests.
+
+            Next steps:
+            - Check out [Getting started with Flutter apps](https://docs.bitrise.io/en/bitrise-ci/getting-started/quick-start-guides/getting-started-with-flutter-projects.html).
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - flutter-installer@%s:
+              inputs:
+              - version: 3.29.2
+          - restore-dart-cache@%s: {}
+          - flutter-test@%s:
+              inputs:
+              - project_location: $BITRISE_FLUTTER_PROJECT_LOCATION
+          - save-dart-cache@%s: {}
+          - deploy-to-bitrise-io@%s: {}
+warnings:
+  flutter: []
+warnings_with_recommendations:
+  flutter: []
+`, flutterIosAndroidVersions...)
