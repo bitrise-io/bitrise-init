d8834310349c7668549cb24a9f62b1048b2f2469
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2026-05-14 14:22:16 +0200

    Python scanner (#308)
    * Python scanner init
    
    * Fix unused hasUnittest
    
    * Remove framework from python configs
    
    * Flutter integration test fix and add missing python manual config
    
    * Install dev requirements
    
    * Add support for uv package manager
    
    * Update go-flutter
    
    * Install missing poetry and uv
    
    * Upgrade pip, use --no-root flag
    
    * Code cleanup
    
    * More cleanup
    
    * Rename poetryPackageModeFalse to poetryPackageModeDisabled

---
+++ _tests/integration/python_test.go
@@ -0,0 +1,191 @@
+package integration
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bitrise-io/bitrise-init/_tests/integration/helper"
+	"github.com/bitrise-io/bitrise-init/models"
+	"github.com/bitrise-io/bitrise-init/steps"
+)
+
+func TestPython(t *testing.T) {
+	var testCases = []helper.TestCase{
+		{
+			Name:             "fastapi-sample",
+			RepoURL:          "https://github.com/bitrise-io/python-samples.git",
+			Branch:           "main",
+			ExpectedResult:   pythonFastapiResultYML,
+			ExpectedVersions: pythonFastapiResultVersions,
+		},
+	}
+
+	helper.Execute(t, testCases)
+}
+
+var pythonFastapiResultVersions = []interface{}{
+	// python-pip-pytest-config
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreVersion,
+	steps.ScriptVersion,
+	steps.ScriptVersion,
+	steps.CacheSaveVersion,
+	steps.DeployToBitriseIoVersion,
+	// python-poetry-pytest-config
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreVersion,
+	steps.ScriptVersion,
+	steps.ScriptVersion,
+	steps.CacheSaveVersion,
+	steps.DeployToBitriseIoVersion,
+	// python-uv-pytest-config
+	models.FormatVersion,
+	steps.ActivateSSHKeyVersion,
+	steps.GitCloneVersion,
+	steps.CacheRestoreVersion,
+	steps.ScriptVersion,
+	steps.ScriptVersion,
+	steps.CacheSaveVersion,
+	steps.DeployToBitriseIoVersion,
+}
+
+var pythonFastapiResultYML = fmt.Sprintf(`options:
+  python:
+    title: Python Project Directory
+    summary: The directory containing the Python project files (requirements.txt,
+      pyproject.toml, etc.)
+    env_key: PYTHON_PROJECT_DIR
+    type: selector
+    value_map:
+      fastapi-pip-sample:
+        config: python-pip-pytest-config
+      fastapi-poetry-sample:
+        config: python-poetry-pytest-config
+      fastapi-uv-sample:
+        config: python-uv-pytest-config
+configs:
+  python:
+    python-pip-pytest-config: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: python
+      workflows:
+        run_tests:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - restore-cache@%s:
+              inputs:
+              - key: pip-{{ checksum "requirements.txt" }}
+          - script@%s:
+              title: Install dependencies
+              inputs:
+              - content: |
+                  #!/usr/bin/env bash
+                  set -euxo pipefail
+
+                  pip install --upgrade pip
+                  pip install -r requirements.txt
+                  pip install -r requirements-dev.txt
+              - working_dir: $PYTHON_PROJECT_DIR
+          - script@%s:
+              title: Run tests
+              inputs:
+              - content: |
+                  #!/usr/bin/env bash
+                  set -euxo pipefail
+
+                  pytest
+              - working_dir: $PYTHON_PROJECT_DIR
+          - save-cache@%s:
+              inputs:
+              - key: pip-{{ checksum "requirements.txt" }}
+              - paths: ~/.cache/pip
+          - deploy-to-bitrise-io@%s: {}
+      tools:
+        python: "3.14"
+    python-poetry-pytest-config: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: python
+      workflows:
+        run_tests:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - restore-cache@%s:
+              inputs:
+              - key: poetry-{{ checksum "poetry.lock" }}
+          - script@%s:
+              title: Install dependencies
+              inputs:
+              - content: |
+                  #!/usr/bin/env bash
+                  set -euxo pipefail
+
+                  pip install poetry
+                  poetry install --no-root
+              - working_dir: $PYTHON_PROJECT_DIR
+          - script@%s:
+              title: Run tests
+              inputs:
+              - content: |
+                  #!/usr/bin/env bash
+                  set -euxo pipefail
+
+                  poetry run pytest
+              - working_dir: $PYTHON_PROJECT_DIR
+          - save-cache@%s:
+              inputs:
+              - key: poetry-{{ checksum "poetry.lock" }}
+              - paths: ~/.cache/pypoetry
+          - deploy-to-bitrise-io@%s: {}
+      tools:
+        python: "3.12"
+    python-uv-pytest-config: |
+      format_version: "%s"
+      default_step_lib_source: https://github.com/bitrise-io/bitrise-steplib.git
+      project_type: python
+      workflows:
+        run_tests:
+          steps:
+          - activate-ssh-key@%s: {}
+          - git-clone@%s: {}
+          - restore-cache@%s:
+              inputs:
+              - key: uv-{{ checksum "uv.lock" }}
+          - script@%s:
+              title: Install dependencies
+              inputs:
+              - content: |
+                  #!/usr/bin/env bash
+                  set -euxo pipefail
+
+                  pip install uv
+                  uv sync
+              - working_dir: $PYTHON_PROJECT_DIR
+          - script@%s:
+              title: Run tests
+              inputs:
+              - content: |
+                  #!/usr/bin/env bash
+                  set -euxo pipefail
+
+                  uv run pytest
+              - working_dir: $PYTHON_PROJECT_DIR
+          - save-cache@%s:
+              inputs:
+              - key: uv-{{ checksum "uv.lock" }}
+              - paths: ~/.cache/uv
+          - deploy-to-bitrise-io@%s: {}
+      tools:
+        python: "3.12"
+warnings:
+  python: []
+warnings_with_recommendations:
+  python: []`,
+	pythonFastapiResultVersions...)
---
+++ scanners/python/detection_test.go
@@ -0,0 +1,78 @@
+package python
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParsePyproject(t *testing.T) {
+	tests := []struct {
+		name    string
+		content string
+		want    pyprojectInfo
+	}{
+		{
+			name: "poetry name only",
+			content: `[tool.poetry]
+name = "my-app"
+version = "0.1.0"
+`,
+			want: pyprojectInfo{poetryName: "my-app"},
+		},
+		{
+			name: "package-mode false",
+			content: `[tool.poetry]
+name = "my-app"
+package-mode = false
+`,
+			want: pyprojectInfo{poetryName: "my-app", poetryPackageModeDisabled: true},
+		},
+		{
+			name: "explicit packages declaration",
+			content: `[tool.poetry]
+name = "my-lib"
+packages = [{ include = "my_lib" }]
+`,
+			want: pyprojectInfo{poetryName: "my-lib", poetryHasPackagesField: true},
+		},
+		{
+			name: "PEP 621 project name only",
+			content: `[project]
+name = "modern-lib"
+`,
+			want: pyprojectInfo{projectName: "modern-lib"},
+		},
+		{
+			name: "both project and tool.poetry",
+			content: `[project]
+name = "modern-lib"
+
+[tool.poetry]
+name = "legacy-name"
+`,
+			want: pyprojectInfo{projectName: "modern-lib", poetryName: "legacy-name"},
+		},
+		{
+			name: "single quotes",
+			content: `[tool.poetry]
+name = 'my-app'
+`,
+			want: pyprojectInfo{poetryName: "my-app"},
+		},
+		{
+			name: "fields outside tool.poetry are ignored",
+			content: `[tool.poetry.dependencies]
+name = "this-is-a-dep-not-the-project"
+`,
+			want: pyprojectInfo{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parsePyproject(tt.content)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
